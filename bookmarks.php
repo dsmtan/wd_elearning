29f283fc1f5c54b7e32fdad0abd28387c984171c
Magdalena Tuła <magdalenatula@MacBook-Pro-Magdalena.local>
2020-05-31 21:01:05 +0200

    fixes bookmark

--- bookmarks.php
+++ bookmarks.php
@@ -1,6 +1,43 @@
 <?php
 include 'includes/autoloader.php';
 
+session_start();
+$userID = $_SESSION['userID'];
+
+$getAllBookmarks = new Bookmark();
+$getAllSegments = new Segment();
+$getAllModules = new Module();
+
+$allBookmarks = $getAllBookmarks->getBookmarks($userID);
+$numberBookmarks = count($allBookmarks);
+
+if ($numberBookmarks === 0){
+    $noBookmarksHTMl .= "<h3>You have no bookmarks</h3> ";
+} else {
+    $bookmarkHTMl = "";
+
+    foreach ($allBookmarks as $bookmark) {
+        if ($numberBookmarks === "0"){
+            $bookmarkHTMl .= "<h3>You have no bookmarks</h3> ";
+        } else {
+            $segmentID = $bookmark->segmentID;
+            $segment = $getAllSegments->getSegmentContent($segmentID);
+            $moduleID = $segment->moduleID;
+            $moduleTitle = $getAllModules->getModuleTitle($moduleID);
+            $moduleNumber = substr($moduleID, -2);
+        
+            $bookmarkHTMl .= "  
+            <a href='module.php?id=$moduleID&segid=$segmentID'>
+            <article>
+            <div><h1>$segment->title</h1></div>
+            <div><p>$moduleNumber $moduleTitle</p></div>
+            </article>
+            </a>
+            ";
+        }
+    }
+
+}
 
 ?>
 
@@ -19,9 +56,12 @@ include 'includes/autoloader.php';
         <?php
         require_once('components/navigation.php');
         ?>
-
         <section class="section--bookmarksContent">
-            <h1>Bookmarks</h1>
+        <h1>Bookmarks</h1>
+        <?= $noBookmarksHTMl ?>
+        <div class='section--bookmarksArticles'>
+        <?= $bookmarkHTMl ?>
+        </div>
         </section>
 
     </main>
