0c495683b3354a5e7b1437260b7e01852f3c7d01
dsmtan <38037729+dsmtan@users.noreply.github.com>
2020-06-02 22:45:54 +0200

    added nav tooltips (#32)

--- components/navigation.php
+++ components/navigation.php
@@ -5,9 +5,9 @@ $learningIcon = file_get_contents('assets/ico_learning_regular.svg');
 $bookmarkIcon = file_get_contents('assets/ico_bookmark_regular.svg');
 
 $navOptions = array(
-    'dashboard'  => array('url' => 'dashboard.php', 'icon' => $dashboardIcon),
-    'learning'  => array('url' => 'index.php', 'icon' => $learningIcon),
-    'bookmarks' => array('url' => 'bookmarks.php', 'icon' => $bookmarkIcon),
+    'dashboard'  => array('name' => 'Dashboard', 'url' => 'dashboard.php', 'icon' => $dashboardIcon),
+    'learning'  => array('name' => 'Learning', 'url' => 'index.php', 'icon' => $learningIcon),
+    'bookmarks' => array('name' => 'Bookmarks', 'url' => 'bookmarks.php', 'icon' => $bookmarkIcon),
 );
 
 $urlPath = $_SERVER['REQUEST_URI'];
@@ -16,7 +16,7 @@ $isModule = strpos(basename($urlPath), 'module.php') === 0;
 $isTest = strpos(basename($urlPath), 'moduletest.php') === 0;
 foreach ($navOptions as $option) {
     $currentClass = basename($urlPath) === $option['url'] || ($isModule || $isTest) && $option['url'] === 'index.php' ? "class='current'" : '';
-    $navLinks .= "<a href='{$option['url']}' $currentClass>{$option['icon']}</a>";
+    $navLinks .= "<a href='{$option['url']}' $currentClass>{$option['icon']}<span>{$option['name']}</span></a>";
 }
 
 $mainNavigation =   "<nav class='nav--main'>
@@ -24,7 +24,7 @@ $mainNavigation =   "<nav class='nav--main'>
                         <div class='div--navLinks'>
                         {$navLinks}
                         </div>
-                        <a href='logout.php'>Log Out</a>
+                        <a class='a--logout' href='logout.php'>Log out</a>
                     </nav>";
 
 echo $mainNavigation;
